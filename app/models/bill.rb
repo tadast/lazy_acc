570b245e650185e447eb4404d79274943dca24ff
titas <titas.norkunas@gmail.com>
2010-04-10 15:53:43 +0300

    bill/trans

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,3 +1,6 @@
 class Bill < ActiveRecord::Base
   belongs_to :bucket
+  has_many :transactions
+
+
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,10 +1,10 @@
 class Transaction < ActiveRecord::Base
-  belongs_to :bucket
-  named_scope :unconfirmed, {:conditions => "status=#{Transaction::STATUS[:unconfirmed]}" }
-
   STATUS = {
     :unconfirmed => 0,
     :confirmed => 1
   }.freeze
+
+  belongs_to :bucket
+  named_scope :unconfirmed, { :conditions => "status=#{Transaction::STATUS[:unconfirmed]}" }
 
 end
