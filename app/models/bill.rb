2ca9f765d73d3e294dbf9dc64cf47e292124c55b
titas <titas.norkunas@gmail.com>
2010-04-10 17:12:08 +0300

    bill

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,12 +1,17 @@
 class Bill < ActiveRecord::Base
   belongs_to :bucket
-  has_many :transactions
-  before_destroy :delete_transactions
+  has_many :transactions, :on_delete => :delete_transactions
+  after_create :create_transaction
 
   named_scope :active, lambda {|*args| {:conditions => ["start_date >= ? AND end_date <= ?", args.first || Date.today, args.first || Date.today]}}
 
   def delete_transactions
     self.transactions.unconfirmed.delete_all
+  end
+
+  def create_transaction
+    self.transactions << Transaction.new(:title => self.title, :amount => self.amount * self.bucket.credit_debet)
+    self.save
   end
 
 end
