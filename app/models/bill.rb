4b572a3e7e402aced925b19033fe90d84e842d34
titas <titas.norkunas@gmail.com>
2010-04-10 16:20:45 +0300

    bill active

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,6 +1,12 @@
 class Bill < ActiveRecord::Base
   belongs_to :bucket
   has_many :transactions
+  before_destroy :delete_transactions
 
+  named_scope :active => lambda {|*args| {:conditions => ["start_date >= ? AND end_date <= ?", args.first || Time.now, args.first || Time.now]}}
+
+  def delete_transactions
+    self.transactions.unconfirmed.delete_all
+  end
 
 end
--- db/migrate/20100410122602_add_status_to_transactions.rb
+++ db/migrate/20100410122602_add_status_to_transactions.rb
@@ -1,6 +1,6 @@
 class AddStatusToTransactions < ActiveRecord::Migration
   def self.up
-    add_column :transactions, :status, :integer
+    add_column :transactions, :status, :integer, :default => Transaction::STATUS[:unconfirmed]
   end
 
   def self.down
