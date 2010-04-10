e89ab875c76f92c48ec3cf4c5454759d596c3dae
Tadas Tamošauskas <TadasTamo@gmail.com>
2010-04-10 20:28:46 +0300

    supushink ir einam gert

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,17 +1,19 @@
 class Bill < ActiveRecord::Base
   belongs_to :bucket
   has_many :transactions
-  after_create :create_transaction
-  before_destroy :delete_transactions
+
   named_scope :active, lambda {|*args| {:conditions => ["start_date >= ? AND end_date <= ?", args.first || Date.today, args.first || Date.today]}}
 
+  after_create :create_transaction
+  before_destroy :delete_transactions
+  
   def delete_transactions
     self.transactions.unconfirmed.delete_all
   end
 
   def create_transaction
-    self.transactions << Transaction.new(:title => self.title, :amount => self.amount * self.bucket.credit_debet)
-    self.save
+    self.bucket.transactions.create(:title => self.title, :amount => self.amount * self.bucket.credit_debet)
+    self.bucket.save
   end
 
 end
