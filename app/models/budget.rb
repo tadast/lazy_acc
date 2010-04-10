97d7333668e50fdeb85b515c3b1afb699d326787
Tadas Tamošauskas <TadasTamo@gmail.com>
2010-04-10 15:12:06 +0300

    qq

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,3 +1,12 @@
 class Budget < ActiveRecord::Base
   belongs_to :bucket
+  named_scope :name, lambda { |*args| { :conditions => ['budget.valid_from => ? AND budget.valid_to <= ? AND budget.bucket_id = ?', args.first] } }
+  
+  def current
+    
+  end
+  
+  def self.create_if_needed(user, bucket)
+    self.
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   acts_as_authentic
   has_many :buckets
+  has_many :budgets, :through => :buckets
+  after_create :create_template_buckets
+  
+  def create_template_buckets
+    Bucket::TEMPLATE.each do |key, value|
+      buck = self.buckets.build(value)
+    end
+    return self.save
+  end
 end
