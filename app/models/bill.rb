247accd7ec8bb3578b2e74611d0badba043f167d
Tadas Tamošauskas <TadasTamo@gmail.com>
2010-04-10 20:18:11 +0300

    fixshit

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,8 +1,8 @@
 class Bill < ActiveRecord::Base
   belongs_to :bucket
-  has_many :transactions, :on_delete => :delete_transactions
+  has_many :transactions
   after_create :create_transaction
-
+  before_destroy :delete_transactions
   named_scope :active, lambda {|*args| {:conditions => ["start_date >= ? AND end_date <= ?", args.first || Date.today, args.first || Date.today]}}
 
   def delete_transactions
